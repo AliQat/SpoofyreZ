0175c9a84f7c85b991c2fb12661dd23d453dc022
aidanjavidpour01973845 <aidan_javidpour@student.uml.edu>
2024-12-05 13:18:19 -0500

    parameter screen created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.android)
+    id("androidx.navigation.safeargs")
 }
 
 android {
--- app/src/main/java/com/mobileapp/spoofyrez/ui/parameters/ParametersFragment.kt
+++ app/src/main/java/com/mobileapp/spoofyrez/ui/parameters/ParametersFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mobileapp.spoofyrez.R
@@ -19,6 +20,7 @@ class ParametersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentParametersBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -29,9 +31,18 @@ class ParametersFragment : Fragment() {
 
     private fun setupClickListeners() {
         binding.btnFindSimilar.setOnClickListener {
-            findNavController().navigate(R.id.action_parameters_to_results)
+            val dance = binding.seekBarDanceability.progress.toInt()
+            val energy = binding.seekBarEnergy.progress.toInt()
+            val speech = binding.seekBarSpeechiness.progress.toInt()
+            val acoustic = binding.seekBarAcousticness.progress.toInt()
+            val instrumental = binding.seekBarInstrumentalness.progress.toInt()
+            val valence = binding.seekBarValence.progress.toInt()
+            val tempo = binding.seekBarTempo.progress.toInt()
+            val action = ParametersFragmentDirections.actionParametersToResults(dance, energy, speech, acoustic, instrumental, valence, tempo)
+            findNavController().navigate(action)
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
